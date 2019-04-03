8444d5751d1eb61fe83f7b6478c4dd6a72db5120
Sarah Creasey <sarahjhcreasey@gmail.com>
2019-04-03 08:25:28 +0000

    First code for the rotation cipher task (minimal progress)

--- main.c
+++ main.c
@@ -1,13 +1,46 @@
 /* Programming Assignment 1: English Test Ciphers
 Created: 3 / 04 / 2019
 Author: Sarah Creasey
+Description:
 */
 
+//int shift (char text[]);
 
 #include <stdio.h>
 
 int main()  {
+    
+char x = 97;
+x = x - 65;
+
+printf("%d \n", x);
 
 return 0;
 
+
+
+
+
+ char text[] = "HELP" ;
+ char newLetter = 0;
+ // char text[] = "HELP" ;
+
+    printf("letter is now %d\n", newLetter); 
+
+    return 0;
+
 }
+
+int shift (char text[])     {
+    char text[] = "HELP" ;
+    char newLetter;      // result of rotation
+    int x = text;
+    int k = 5;     // degree of rotation
+
+    newLetter = x + k;
+    
+    return newLetter;
+}
+
+
+*/
