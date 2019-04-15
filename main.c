80ae95fe5b8e8d050de8740fc098a7ab70433190
Sarah Creasey <sarahjhcreasey@gmail.com>
2019-04-15 08:30:42 +0000

    Encryption and decryption function with key

--- main.c
+++ main.c
@@ -1,50 +1,90 @@
-/* Programming Assignment 1: English Test Ciphers
+/* Programming Assignment 1: English Text Ciphers
 Created: 3 / 04 / 2019
 Author: Sarah Creasey
 Description:
 */
 
 #include <stdio.h>
-#include <string.h>     // functions within library can be used to find the length of a string
+#include <string.h>     // note: contains string length function
+#include <ctype.h>
 
 
-//  CIPHER FUNCTION PROTOTYPE
-char caesarCipher(char* plainText, int shift);
+// ENCRYPTION CIPHER FUNCTION PROTOTYPE
+char encryption(char plainText[]);
+
+
+// DECRYPTION CIPHER FUNCTION DEFINITION
+char decryption(char plainText[]);
+
 
 
 int main()  {
+char plainText[] = "This is a top secret message";
 
-char plainText[101] = "HELLO";
-char cipherResult = 0;
-
-//scanf("%s", plainText[1] );
+//scanf("%s", plainText[] );
 // newCustom run will read text from the plainText file which essentially performs a scanf
 
-cipherResult = caesarCipher(plainText[101]);
 
-printf("The cipher result text is: \n");
-printf("%c\n", cipherResult);
+printf("%s\n", plainText);      // to test if the value of caeserCipher changes - debugging purposes
 
+encryption(plainText);
+
+//decryption(plainText);
+
+printf("Cipher result: \n");
+printf("%s\n", plainText);
 
 return 0;
-}
-
-//  CIPHER FUNCTION DEFINITION
-char caesarCipher(char * plainText, int key)    {
-
-int shift = 1;
-char cipherResult;
-
-
-for (int index = 0; index <= strlen(plainText) ; index ++)   {
-    
-    cipherResult = (plainText[index] + shift) % 26;    // will return ciphered letter as a number
-    
-    return cipherResult;
-    
-}
 
 }
+
+// ENCRYPTION CIPHER FUNCTION DEFINITION
+char encryption(char *plainText)    {
+
+        for (int index = 0; index <= strlen(plainText) ; index ++)   {
+               
+            int shift = 6; 
+            plainText[index] = plainText[index] + shift;  
+        
+                 if ( plainText[index] >= 97 )  {
+                     plainText[index] = plainText[index] - 32;
+                 }
+                 
+                 if ( plainText[index] == (32 + shift) ) {
+                            plainText[index] = plainText[index] - shift;
+                }
+         }   
+         
+         return *plainText;
+}
+
+// DECRYPTION CIPHER FUNCTION DEFINITION
+char decryption(char plainText[])   {
+    
+        for (int index = 0; index <= strlen(plainText) ; index ++)   {
+                   
+                int shift = 6; 
+                plainText[index] = plainText[index] - shift;  
+                // if c - k < 0 add 26 to the result 
+            
+                     if ( plainText[index] >= 97 )  {
+                         plainText[index] = plainText[index] - 32;
+                     }
+                     
+                     if ( plainText[index] == (32 + shift) ) {
+                                plainText[index] = plainText[index] - shift;
+                    }
+             }   
+             
+             return *plainText;
+            
+}
+
+
+
+
+
+
 
 
 
