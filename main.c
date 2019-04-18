5aaee8cadfee808a2e482df41e7a754115376a39
sarahcreasey <48225721+sarahcreasey@users.noreply.github.com>
2019-04-18 14:06:26 +1000

    Update main.c

--- main.c
+++ main.c
@@ -11,9 +11,9 @@ Description:
 /*  TO DO
 successfully read text string from input output file
 use menu system with switch case statements
- -- Do not encrypt white space, punctuation, or numerals (if input character is not a letter it should
-be copied to the output unmodied)
-
+Do not encrypt white space, punctuation, or numerals (if input character is not a letter it should
+be copied to the output unmodied) -- maybe use the ispunct() function?
+modify rotation functions to have a second input which is the shift key
 */
 
 
@@ -30,11 +30,11 @@ char encryptionSubstitution(char plainText[]);
 int main()  {
     
     printf(" Select an option from the following menu: \n");
-    printf("1 Encryption with a rotation cipher given input text and key \n");
-    printf("2 Decryption with a rotation cipher given input text and key \n");
-    printf("3 Encryption with a substitution cipher given input text and key \n");
-    printf("4 Decryption with a substitution cipher given input text and key \n");
-    printf("5 Decryption with a rotation cipher given the input text \n");
+    printf("1 - Encryption with a rotation cipher given input text and key \n");
+    printf("2 - Decryption with a rotation cipher given input text and key \n");
+    printf("3 - Encryption with a substitution cipher given input text and key \n");
+    printf("4 - Decryption with a substitution cipher given input text and key \n");
+    printf("5 - Decryption with a rotation cipher given the input text \n");
     
     int menuSelection = 0;
     scanf("%d", &menuSelection);
@@ -46,7 +46,7 @@ int main()  {
     
     //scanf("%s", plainText);  // newCustom run will read text from the plainText file which essentially performs a scanf
     //scanf("%d", &key);
-   char plainText[] = "The quick brown fox jumped over the lazy dog";
+   char plainText[] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
     //char plainText[] = "ZNK WAOIQ HXUCT LUD PASVKJ UBKX ZNK RGFE JUM";
     
     //printf("%d", key);
@@ -150,34 +150,61 @@ char encryptionSubstitution(char plainText[])   {
 
          for (int index = 0; index <= strlen(plainText) && plainText[index] != '\0'; index ++)   {
              
-    //note: the substitution key is bases off QWERTY, the keyboard design for Latin alphabets
-           switch(plainText[index])
-           {      case 'A': *plainText = 'Q';
+    //note: the substitution key is bases off QWERTY, the keyboard design for the Latin alphabet
+           switch(plainText[index])      {
+                  case 'A': *plainText = 'Q';
+                    break;
                   case 'B': *plainText = 'W';
+                    break;
                   case 'C': *plainText = 'E';
+                    break;
                   case 'D': *plainText = 'R';
+                    break;
                   case 'E': *plainText = 'T';
+                    break;
                   case 'F': *plainText = 'Y';
+                    break;
                   case 'G': *plainText = 'U';
+                    break;
                   case 'H': *plainText = 'I';
+                    break;
                   case 'I': *plainText = 'O';
+                    break;
                   case 'J': *plainText = 'P';
+                    break;
                   case 'K': *plainText = 'A';
+                    break;
                   case 'L': *plainText = 'S';
+                    break;
                   case 'M': *plainText = 'D';
+                    break;
                   case 'N': *plainText = 'F';
+                    break;
                   case 'O': *plainText = 'G';
+                    break;
                   case 'P': *plainText = 'H';
+                    break;
                   case 'Q': *plainText = 'J';
+                    break;
                   case 'R': *plainText = 'K';
+                    break;
                   case 'S': *plainText = 'L';
+                    break;
                   case 'T': *plainText = 'Z';
+                    break;
                   case 'U': *plainText = 'X';
+                    break;
                   case 'V': *plainText = 'C';
-                  case 'W': *plainText = 'V';
+                    break;
+                  case 'W': *plainText = 'V';  
+                    break;
                   case 'X': *plainText = 'B';
+                    break;
                   case 'Y': *plainText = 'N';
+                    break;
                   case 'Z': *plainText = 'M';
+                    break;
+                  default: printf("Something went wrong \n");
         
                 }  
                   
